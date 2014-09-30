15da259debcc0b37433f639e46ce640ef5b01643
ButenkoT <tanya@butenko.me>
2014-09-30 16:32:13 +1000

    more of movie 2

--- tanya_butenko/week2/movie/main.rb
+++ tanya_butenko/week2/movie/main.rb
@@ -5,9 +5,16 @@ require 'uri'
 
 get '/' do
 	unless params[:movie_poster].nil?
-	title = URI.escape(params['movie_poster'])
-	movie_results = HTTParty.get("http://www.omdbapi.com/?i=&t=#{ title }") # Fetch the movie data
-	@movie = JSON.parse(movie_results)['Poster'] # Convert the raw 
+		title = URI.escape(params['movie_poster'])
+		movie_results = HTTParty.get("http://www.omdbapi.com/?i=&t=#{ title }") # Fetch the movie data
+		@movie = JSON.parse(movie_results)['Poster'] # Convert the raw 
+		
+		@movie = @movie['Search']
+
+		@results = {}
+		@movie.each do |a|
+			@results[a['Title']] = a['imdbID']
+		end
 	end
 
 	erb :movies
